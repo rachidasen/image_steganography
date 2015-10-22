65983616fea044485dd7f42dac4690979234040e
Raghib Ahsan <md.raghib@live.com>
2015-10-22 19:25:38 +0530

    Image Scaled Down

--- image_scale.py
+++ image_scale.py
@@ -1,3 +1,20 @@
-import Image
-im = Image.open("image.jpg");
-print im.format, im.size, im.mode
+import cv2
+import cv2.cv as cv
+filename = "image.jpg"
+oriimage = cv2.imread(filename)
+newx,newy = oriimage.shape[1]/4,oriimage.shape[0]/4 #new size (w,h)
+newimage = cv2.resize(oriimage,(newx,newy))
+cv2.imwrite("new.jpg",newimage)
+# cv.SaveImage("new.jpg", newimage)
+im = cv.LoadImage("new.jpg")
+# displaying the matrix form of image
+for i in range(im.height):
+    for j in range(im.width):
+        print im[i,j],
+
+    print "\n",i	
+
+
+# cv2.imshow("original image",oriimage)
+cv2.imshow("resize image",newimage)
+# cv2.waitKey(0)
