b17c48f46b13de3c7c802a101b8a5e7f91080819
Raghib Ahsan <md.raghib@live.com>
2016-01-19 17:12:31 +0530

    Converted the sender_ file correct.py to work for 8 bit ascii

--- main.py
+++ main.py
@@ -18,26 +18,26 @@ if args.add:
 			flag=1
 		else:
 			print "reading_file",u
-			with open(u,'r') as myfile:
-				message=myfile.read().replace('\n','')
-				print message
+			# with open(u,'r') as myfile:
+			message=u
+			print message
 
-				# message.encode('unicode-escape')
-				# message.encode("ascii","ignore")
-				# message.dencode('string-escape')
-				# mess=str(message)
-				# print type(mess)
-				# cmd = 'python image_scale.py '+message+' '+str(name_count)+' '
-				cmd='python image_scale.py '+image_name+' "'+message+'" ' + str(name_count)
-				# cmd=str(cmd)
-				print cmd
-				cmd.decode('string-escape')
-				# print cmd
-				os.system(cmd)
-				print "Allah is the forgiver"
-				# os.system("python image_scale.py "+mess+' '+str(name_count))
-				# print u
-				cmd='python de_stgano.py '+image_name+" "+str(name_count)
-				os.system(cmd)
+			# message.encode('unicode-escape')
+			# message.encode("ascii","ignore")
+			# message.dencode('string-escape')
+			# mess=str(message)
+			# print type(mess)
+			# cmd = 'python image_scale.py '+message+' '+str(name_count)+' '
+			cmd='python correct.py '+image_name+' "'+message+'" ' + str(name_count)
+			# cmd=str(cmd)
+			print cmd
+			cmd.decode('string-escape')
+			# print cmd
+			os.system(cmd)
+			print "Allah is the forgiver"
+			# os.system("python image_scale.py "+mess+' '+str(name_count))
+			# print u
+			# cmd='python de_stgano.py '+image_name+" "+str(name_count)
+			# os.system(cmd)
 			name_count+=1
-			myfile.close()
+			# myfile.close()
